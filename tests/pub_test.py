69f37744a23bc7de5c5dad7b2cced11ee9f955fa
fatpav <fat_pav@hotmail.com>
2020-09-16 13:06:22 +0100

    Added test for customer name

--- tests/pub_test.py
+++ tests/pub_test.py
@@ -1,15 +1,18 @@
-
 import unittest
 from src.pub import Pub 
 
 class TestPub(unittest.TestCase):
     def setUp(self):
         self.pub = Pub("Ox", 100.00)
+        self.customer = Customer("Billy", 50.00)
 
     def test_pub_has_name(self):
         self.assertEqual("Ox", self.pub.name)
 
     def test_pub_has_cash(self):
         self.assertEqual(100.00, self.pub.cash)
+
+    def test_customer_has_name(self):
+        self.assertEqual("Billy", self.customer.name)
 
 
