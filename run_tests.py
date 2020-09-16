de53d42adb111cb6e4e8375dcf40eeb5db0a27b4
fatpav <fat_pav@hotmail.com>
2020-09-16 12:54:34 +0100

    initial commit

---
+++ run_tests.py
@@ -0,0 +1,6 @@
+import unittest
+from tests.pub_test import TestPub
+
+if __name__ == "__main__": # __name__ is a global keyword that asks where a function is running
+    unittest.main()
+
---
+++ tests/pub_test.py
@@ -0,0 +1,14 @@
+
+import unittest
+from src.pub import Pub 
+
+class TestPub(unittest.TestCase):
+    def setUp(self):
+        self.pub = Pub("Ox", 100.00)
+
+    def test_pub_has_name(self):
+        self.assertEqual("Ox", self.pub.name)
+
+    def test_pub_has_cash(self):
+        
+
