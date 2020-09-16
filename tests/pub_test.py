5c1cc4d0787258557965deff331a6193c1d7d876
fatpav <fat_pav@hotmail.com>
2020-09-16 13:40:28 +0100

    5th commit. All basic tests written and passed.  Classes built

--- tests/pub_test.py
+++ tests/pub_test.py
@@ -1,10 +1,13 @@
 import unittest
 from src.pub import Pub 
+from src.customer import Customer
+from src.drinks import Drinks
 
 class TestPub(unittest.TestCase):
     def setUp(self):
         self.pub = Pub("Ox", 100.00)
         self.customer = Customer("Billy", 50.00)
+        self.drinks = Drinks("Beer", 5.00)
 
     def test_pub_has_name(self):
         self.assertEqual("Ox", self.pub.name)
@@ -15,8 +18,8 @@ class TestPub(unittest.TestCase):
     def test_customer_has_name(self):
         self.assertEqual("Billy", self.customer.name)
     
-    def test_customer_has_cash(self):
-        self.assertEqual(50.00, self.customer.cash)
+    def test_customer_has_wallet(self):
+        self.assertEqual(50.00, self.customer.wallet)
 
     def test_drink_has_name(self):
         self.assertEqual("Beer", self.drinks.name)
@@ -25,4 +28,4 @@ class TestPub(unittest.TestCase):
         self.assertEqual(5.00, self.drinks.price)
 
     def customer_buy_drink(self):
-        self.assertEqual()
+        pass
